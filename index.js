9dd4d86f76df1667e005001db073bcade2836c77
TheCow <cow2cow4cow@gmail.com>
2018-05-26 16:59:36 -0400

    wasd

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = requre("discord.js");
 const bot = new Discord.Client();
 
+bot.login("NDUwMDM2Mjc0MjQ2NDUxMjIx.DetYxw.XwEk8MUqQi7YAXaVq1NPI0GrksU");
+
 bot.on("ready", function() {
     console.log("JankBot is ready!");
 });
