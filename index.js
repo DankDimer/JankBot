accc4280aca16b43bc76136a0291c622c6b5bb2f
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-05-26 18:00:16 -0400

    test

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ bot.login("NDUwMDM2Mjc0MjQ2NDUxMjIx.DetYxw.XwEk8MUqQi7YAXaVq1NPI0GrksU");
 bot.on('ready', () => {
 
   })
-bot.on("ready", function() {
-    console.log("JankBot is ready!");
+  client.on("ready", () => {
+ 
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  
+    client.user.setActivity(`with fire.`);
 
 });
 
