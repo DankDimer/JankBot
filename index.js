9043efb1c95ddb309dab49a72b3368f2cf5cb41a
DankDimer <39657674+DankDimer@users.noreply.github.com>
2018-05-26 18:10:25 -0400

    big changes

--- index.js
+++ index.js
@@ -1,38 +1,109 @@
+
 const Discord = require("discord.js");
-const bot = new Discord.Client();
 
-bot.login("NDUwMDM2Mjc0MjQ2NDUxMjIx.DetYxw.XwEk8MUqQi7YAXaVq1NPI0GrksU");
-bot.on('ready', () => {
 
-  })
-bot.on("ready", function() {
-    console.log("JankBot is ready!");
-client.user.setActivity("with fire.")
+const client = new Discord.Client();
+
+
+const config = require("./config.json");
+
+
+client.on("ready", () => {
+ 
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  client.user.setActivity(`with fire.`);
+
 });
 
-bot.on("message", message => {
-    if (message.content === "kill me") {
-        message.reply("Here are the nearest bridges: https://www.google.com/maps/search/bridge/");
-    }
-    if (message.content === "this server sucks") {
-        message.reply("And you swallow, dipshit.");
-    }
-    if (message.content === "daddy") {
-        message.reply("I'm daddy you fool.");
-    }
-    if (message.content === "Knoddy is cool.") {
-        message.reply("True.");
-    }
-    if (message.content === "DankDimer sucks.") {
-        message.reply("Fuck you.");
-    }
-    if (message.content === "DankDimer is cool.") {
-        message.reply("True.");
-    }
-    if (message.content === "Knoddy sucks.") {
-        message.reply("Fuck you.");
-    }
-    if (message.content === "help") {
-        message.reply("JankBot is currently in development. This command was not fully added.")
-    }
-})
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+  
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  
+  if(command === "ping") {
+
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+  
+  if(command === "say") {
+
+    const sayMessage = args.join(" ");
+
+    message.delete().catch(O_o=>{}); 
+
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "ban") {
+    if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "purge") {
+    const deleteCount = parseInt(args[0], 10);
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    const fetched = await message.channel.fetchMessages({count: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+
+  
+if(command === "help") {
+
+  message.channel.send("JankBot is currently in development.")
+}
+});
+
+
+
+
+
+
+client.login(config.token);
+           
