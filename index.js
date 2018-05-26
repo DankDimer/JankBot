87079bb1b88e3561e76eca5a8b870fb47461d11b
TheCow <cow2cow4cow@gmail.com>
2018-05-26 17:22:10 -0400

    wasdwasdwasd

--- index.js
+++ index.js
@@ -17,4 +17,5 @@ bot.on("message", message => {
     if (message.content === "daddy") {
         message.reply("I'm daddy you fool.");
     }
+    message.reply("wasdwasdwasdwasdwasdwasdwasdwasd");
 })
