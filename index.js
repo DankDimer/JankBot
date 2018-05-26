30a1edaa7424e1e09dbff231df90d58566b94683
TheCow <cow2cow4cow@gmail.com>
2018-05-26 16:59:08 -0400

    Beautifulllll

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const Discord = requre("discord.js");
 const bot = new Discord.Client();
 
 bot.on("ready", function() {
-    console.log("WASD");
+    console.log("JankBot is ready!");
 });
+
+bot.on("message", message => {
+    if (message.content === "kill me") {
+        message.reply("Here are the nearest bridges:");
+    }
+})
